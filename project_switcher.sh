352909f84117200f68bacf531572d8ef7dd57e63
Alex Jones <alexjajones1@gmail.com>
2025-02-24 21:44:28 +0000

    Improved the way project names are displayed

--- project_switcher.sh
+++ project_switcher.sh
@@ -1,10 +1,10 @@
 source ~/.config/tmux/project_roots.sh
 
-selected=$(echo $PROJECT_ROOTS | xargs ls -d | grep -v \"^$(tmux display-message -p '#S')\$\" | fzf --reverse)
-selected_name=$(echo $selected | tr "." -)
+selected=$(echo $PROJECT_ROOTS | xargs ls -d | sed "s:$HOME/::" | sed 's:/*$::' | fzf --reverse)
+selected_name=$(echo $selected | rev | cut -d'/' -f1 | rev)
 
 if [ "$selected_name" = "" ]; then
     echo "skipping"
 else
-	tmuxinator start $selected_name -n $selected_name -p ~/.config/tmux/default.yml workspace="${selected}"
+	tmuxinator start $selected_name -n $selected_name -p ~/.config/tmux/default.yml workspace="~/${selected}"
 fi
